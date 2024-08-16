3683c97fd91fd5dbdcfb38b8b11c133fbfa0baac
Kshitij Bajracharya <bkshitij.dev@gmail.com>
2024-08-16 12:50:10 +0545

    Update main class name and mapper scan

--- src/main/java/com/thedevjournal/mystocks/MyStocksApplication.java
+++ src/main/java/com/thedevjournal/mystocks/MyStocksApplication.java
@@ -5,11 +5,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan("com.example.share.mapper")
-public class ShareApplication {
+@MapperScan("com.thedevjournal.mystocks.mapper")
+public class MyStocksApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ShareApplication.class, args);
+		SpringApplication.run(MyStocksApplication.class, args);
 	}
 
 }
